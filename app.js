f7bf4e237f92383b694a93585803def83dcf4354
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-12 21:39:36 +0100

    Refactor using JSX

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-const title = React.createElement(
-    'h1',
-    { id:'main-title', title: 'This is a title.' },
-    'My first React Element!'
-);
-
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it into the DOM.'
-);
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
-);
+const desc = 'I just learned how to create a React node and render it into the Dom.';
+const myTitleID = 'main-title'
+const name = 'Michal';
+const header = (
+    <header>
+        <h1 id={myTitleID}>{name}'s First React Element!</h1>;
+        <p>{desc}</p>;
+    </header>
+)
 
 ReactDOM.render(
     header,
@@ -24,4 +15,4 @@ ReactDOM.render(
 
 
 
-console.log(title);
+// console.log(title);
