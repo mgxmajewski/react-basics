09022d7c4576921302ac0dad9d83d08f8d65a237
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-12 21:48:22 +0100

    Replace example code with Header component

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const desc = 'I just learned how to create a React node and render it into the Dom.';
-const myTitleID = 'main-title'
-const name = 'Michal';
-const header = (
-    <header>
-        <h1 id={myTitleID}>{name}'s First React Element!</h1>;
-        <p>{desc}</p>;
-    </header>
-)
-
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 ReactDOM.render(
     header,
     document.getElementById('root')
