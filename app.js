5e1d592bc9ed80fb2d349361d588ad5ba8ef8cc2
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-13 00:08:54 +0100

    Add Player component

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Header() {
+const Header = () => {
     return (
         <header>
             <h1>Scoreboard</h1>
@@ -6,8 +6,25 @@ function Header() {
         </header>
     );
 }
+
+const Player = () => {
+    return (
+        <div className='player'>
+            <span className="player-name">
+                Guil
+            </span>
+
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score"> 35 </span>
+                <button className="counter-action increment"> + </button>
+            </div>
+        </div>
+    );
+}
+
 ReactDOM.render(
-    header,
+    <Player />,
     document.getElementById('root')
 );
 
