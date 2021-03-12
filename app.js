b2f145fced1dfc96aa9a3b0421f45be5f99f4f2d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-12 20:16:20 +0100

    Add desc and header.

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const title = React.createElement(
     'My first React Element!'
 );
 
-ReactDOM.render(
+const desc = React.createElement(
+    'p',
+    null,
+    'I just learned how to create a React node and render it into the DOM.'
+);
+
+const header = React.createElement(
+    'header',
+    null,
     title,
+    desc
+);
+
+ReactDOM.render(
+    header,
     document.getElementById('root')
 );
+
+
 
 console.log(title);
