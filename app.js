a1f4246d50f1d7e748c75a32329a08d6628ba4c7
mgxmajewski <mgxmajewski@BIVTR0M.localdomain>
2021-03-13 02:39:06 +0100

    Refactor this binding with arrow function

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ class Counter extends React.Component {
        score: 0
     };
 
-    incrementScore(state) {
+    incrementScore = () => {
         this.setState({
             score: this.state.score + 1
         });
@@ -56,7 +56,7 @@ class Counter extends React.Component {
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
                 <span className="counter-score"> { this.state.score } </span>
-                <button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
+                <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
         );
     }
