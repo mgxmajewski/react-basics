34c6b1ebd03e25eb0b99d7d32df95cefe632dd92
mgxmajewski <mgxmajewski@BIVTR0M.localdomain>
2021-03-13 02:23:42 +0100

    Add state to counter

--- app.js
+++ app.js
@@ -35,19 +35,22 @@ const Player = (props) => {
             <span className="player-name">
                 { props.name }
             </span>
-            <Counter
-                score={props.score}
-            />
+            <Counter />
         </div>
     );
 }
 
 class Counter extends React.Component {
+
+    state = {
+       score: 0
+    };
+
     render () {
         return (
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
-                <span className="counter-score"> { this.props.score } </span>
+                <span className="counter-score"> { this.state.score } </span>
                 <button className="counter-action increment"> + </button>
             </div>
         );
@@ -65,7 +68,6 @@ const App = (props) => {
             {props.initialPlayers.map( player =>
                 <Player
                     name={player.name}
-                    score={player.score}
                     key={player.id.toString()}
                 />
             )}
